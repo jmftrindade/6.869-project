c3df43b1218ead2b7f211c0ea4aab81e7b3753e2
Joana Trindade <jmftrindade@gmail.com>
2017-12-06 22:44:27 -0500

    make sure show_class_for_images script can handle single digit class numbers correctly

--- src/show_class_for_image.sh
+++ src/show_class_for_image.sh
@@ -7,6 +7,6 @@ miniplaces_data_dir=$miniplaces_dir/data
 images_dir=$miniplaces_data_dir/images/
 
 class_number=`grep $image $miniplaces_data_dir/*txt | cut -d' ' -f2`
-class=`grep " $class_number" $miniplaces_data_dir/categories.txt | cut -d' ' -f1 | cut -d'/' -f3`
+class=`grep " $class_number$" $miniplaces_data_dir/categories.txt | cut -d' ' -f1 | cut -d'/' -f3`
 echo "class=$class"
 echo "class_number=$class_number"
