f3a9abcb1152708fddb7a5467cfa654201ae32b5
Joana Trindade <jmftrindade@gmail.com>
2017-12-06 22:32:47 -0500

    now using top4 most activated units

--- src/regenerate_kmeans_clusters.sh
+++ src/regenerate_kmeans_clusters.sh
@@ -2,7 +2,7 @@
 
 d='../externals/miniplaces_challenge/data/images/'
 
-for f in feature_vectors/val*ones*txt; do
+for f in feature_vectors/val*txt; do
   cmd="python cluster_by_feature_vector-kmeans.py -i $d -fv $f > clusters-kmeans/$(basename $f)"
   echo "Running \"$cmd\""
   eval $cmd
